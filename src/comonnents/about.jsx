ece12a2580ad42dcee5c361da01bff72fe699b9e
urvesh179 <59966547+urvesh179@users.noreply.github.com>
2024-04-06 23:27:45 +0530

    Update about.jsx

--- src/comonnents/about.jsx
+++ src/comonnents/about.jsx
@@ -15,7 +15,7 @@ export const About=()=> {
                 <div className="ab-info">
                     <div className="ab-info-pic">
                         <h3><UserOutlined/><span>My Name</span></h3>
-                        <img src="https://www.w3schools.com/w3images/avatar_hat.jpg"/>
+                        <img src="https://www.w3schools.com/w3images/avatar_hat.jpg" alt="" />
                     </div>
                     <div className="ab-info-data">Welcome to my website. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
                 </div>
